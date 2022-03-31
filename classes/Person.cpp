37c65eac2bc764350c3bfba2076536ef12bd4ec5
Matt <matthew.procopio.19@cnu.edu>
2022-03-31 18:26:35 -0400

    push

--- classes/Person.cpp
+++ classes/Person.cpp
@@ -12,5 +12,90 @@
 
 //TODO your includes here 
 
+#include "../includes/Person.h"
+#include "../includes/externs.h"
+#include "../includes/constants.h"
+#include "../includes/Soupline.h"
+
+using namespace std;
 
 //TODO your implementation here
+
+/***
+ * Constructor
+ * @param personID identity of this person, save it
+ */
+Person::Person(int personID) {
+	this->personID = personID;
+	this->numbBowlsSoupEaten = 0;
+	this->numbDrinksDrunk = 0;
+}
+
+/***
+ * gets soup and drinks until SoupLine runs out of both then exits
+ *
+ * runs a loop repeatedly asking for soup and drinks from msl, the soupline global
+ * calls msl.getSoup(personID)
+ * If call returns NOT_YOUR_TURN then gives other threads a chance by waiting MILLI_SECONDS_TO_WAIT
+ * If call returns BOWL_OF_SOUP then increment numbBowlsSoupEaten
+ * If call returns OUT_OF_SOUP then stop asking for soup
+ *
+ * calls msl.getDrink(personID)
+ * If call returns NOT_YOUR_TURN then gives other threads a chance by waiting MILLI_SECONDS_TO_WAIT
+ * If call returns DRINK then increment numbDrinksDrunk
+ * If call returns OUT_OF_DRINKS then stop asking for drinks
+ *
+ * If soupline is out of both soup and drinks this function exits
+ * @return void
+ */
+void Person::eatlunch() {
+	bool hasSoup;
+	bool hasDrink;
+	if (msl.getnumbBowlsLeft() > 0) {
+		hasSoup = true;
+	}
+	else {
+		hasSoup = false;
+	}
+	if (msl.getnumbDrinksLeft() > 0) {
+		hasDrink = true;
+	}
+	else {
+		hasDrink = false;
+	}
+
+	//While there is soup and drinks left//
+	while (hasSoup == true || hasDrink == true) {
+
+		//If there is soup left, get soup//
+		if (hasSoup == true) {
+			int soup = msl.getSoup(personID);
+			if (soup == NOT_YOUR_TURN) {
+				this_thread::sleep_for(chrono::milliseconds(MILLI_SECONDS_TO_WAIT));
+			}
+			if (soup == BOWL_OF_SOUP) {
+				numbBowlsSoupEaten++;
+				this_thread::sleep_for(chrono::milliseconds(MILLI_SECONDS_TO_WAIT));
+			}
+			if (soup == OUT_OF_SOUP) {
+				hasSoup = false;
+			}
+		}
+
+		//If there are drinks left, get drink//
+		if (hasDrink == true) {
+			int drink = msl.getDrink(personID);
+			if (drink == NOT_YOUR_TURN) {
+				this_thread::sleep_for(chrono::milliseconds(MILLI_SECONDS_TO_WAIT));
+			}
+			if (drink == DRINK) {
+				numbDrinksDrunk++;
+				this_thread::sleep_for(chrono::milliseconds(MILLI_SECONDS_TO_WAIT));
+			}
+			if (drink == OUT_OF_DRINKS) {
+				hasDrink = false;
+			}
+		}
+
+	}
+}
--- classes/Soupline.cpp
+++ classes/Soupline.cpp
@@ -8,4 +8,154 @@
 #include <mutex>
 //TODO your includes here 
 
+#include "../includes/Soupline.h"
+#include "../includes/externs.h"
+#include "../includes/Person.h"
+#include "../includes/constants.h"
+
+using namespace std;
+
 //TODO your implementation here
+
+    /***
+	 * Simulate a fair soupline, which makes sure everyone gets the same amount of soup and drinks
+	 * @param numbBowlsSoup  start with this many bowls soup
+	 * @param numbDrinks     start with this many drinks
+	 */
+	Soupline::Soupline(int numbBowlsSoup, int numbDrinks) {
+		this->numbBowlsSoupLeft = numbBowlsSoup;
+		this->numbDrinksLeft = numbDrinks;
+	}
+
+	/***
+	 * Sets numbBowlsSoupLeft and numbDrinksLeft values to numbBowlsSoup and numbDrinks
+	 * @param numbBowlsSoup
+	 * @param numbDrinks
+	 */
+	void Soupline::restock(int numbBowlsSoup, int numbDrinks) {
+		this->numbBowlsSoupLeft += numbBowlsSoup;
+		this->numbDrinksLeft += numbDrinks;
+	}
+
+	/***
+	 * If no soup left returns OUT_OF_SOUP
+	 * If personID not found in my_customers AND numbBowlsSoupLeft>0 then give this person a bowl of soup (return BOWL_OF_SOUP)
+	 * and record it by creating new customer struct using personID, numbBowlsSoup=1 and adding this struct to my_customers, be sure to decrement numbBowlsSoupLeft.
+	 * If personID has had more bowls of soup than others in my_customers then return NOT_YOUR_TURN
+	 * If it's persons turn to have soup returns 1 bowl by decrementing numbBowlsSoupLeft and returning BOWL_OF_SOUP
+	 * @return NOT_YOUR_TURN if others have not had as much as this person
+	 *         BOWL_OF_SOUP  1 bowl of soup
+	 *         OUT_OF_SOUP if numbBowlsSoupLeft==0 return this
+	 */
+	int Soupline::getSoup(int personID) {
+		lock_guard<mutex> lock(soup_mutex);
+		if (numbBowlsSoupLeft <= 0) {
+			return OUT_OF_SOUP;
+		}
+		bool inVector = false;
+		int count = 0;
+		int index = 0;
+		for (customer c : my_customers) {
+			if (c.personID == personID) {
+				index = count;
+				inVector = true;
+			}
+			count++;
+		}
+		if (!inVector) {
+			customer c;
+			c.personID = personID;
+			c.numbBowlsSoup = 1;
+			numbBowlsSoupLeft--;
+			my_customers.push_back(c);
+			return BOWL_OF_SOUP;
+		}
+
+		//int test1 = getFewestBowlsOfSoupServedToACustomer();
+		//int test2 = my_customers[index].numbBowlsSoup;
+
+		if (getFewestBowlsOfSoupServedToACustomer() < my_customers[index].numbBowlsSoup) {
+			return NOT_YOUR_TURN;
+		}
+		my_customers[index].numbBowlsSoup++;
+		numbBowlsSoupLeft--;
+		return BOWL_OF_SOUP;
+	}
+
+	/***
+	 * Find customer who has had the fewest bowls of soup and return numbBowlsSoup they have had
+	 * If my_customers is empty, return ZERO
+	 * @return ZERO or int
+	 */
+	int Soupline::getFewestBowlsOfSoupServedToACustomer() {
+		if (my_customers.empty()) {
+			return ZERO;
+		}
+		int fewest = my_customers[0].numbBowlsSoup;
+		for (customer c : my_customers) {
+			if (c.numbBowlsSoup < fewest) {
+				fewest = c.numbBowlsSoup;
+			}
+		}
+		return fewest;
+	}
+
+	/***
+	 * If no drinks left returns OUT_OF_DRINKS
+	 * If personID not found in my_customers AND numbDrinksLeft>0 then give this person a drink (return DRINK)
+	 * and record it by creating new customer struct using personID, numbDrinks=1 and adding this struct to my_customers, be sure to decrement numbDrinksLeft.
+	 * If personID has had more drinks than others in my_customers then return NOT_YOUR_TURN
+	 * If it's persons turn to have drink, then returns 1 drink by decrementing numbDrinksLeft, updating this persons info in my_customers and returning DRINK
+	 * @return NOT_YOUR_TURN if others have not had as much as this person
+	 *         DRINK  1 drink
+	 *         OUT_OF_DRINKS if numbDrinksLeft==0 return this
+	 */
+	int Soupline::getDrink(int personID) {
+		lock_guard<mutex> lock(drink_mutex);
+		if (numbDrinksLeft <= 0) {
+			return OUT_OF_DRINKS;
+		}
+		bool inVector = false;
+		int count = 0;
+		int index = 0;
+		for (customer c : my_customers) {
+			if (c.personID == personID) {
+				index = count;
+				inVector = true;
+			}
+			count++;
+		}
+		if (!inVector) {
+			customer c;
+			c.personID = personID;
+			c.numbDrinks = 1;
+			numbDrinksLeft--;
+			my_customers.push_back(c);
+			return DRINK;
+		}
+
+		if (getFewestDrinksServedToACustomer() < my_customers[index].numbDrinks) {
+			return NOT_YOUR_TURN;
+		}
+		my_customers[index].numbDrinks++;
+		numbDrinksLeft--;
+		return DRINK;
+	}
+
+	/***
+	 * Find customer who has had the fewest drinks and return numbDrinks they have had
+	 * If my_customers is empty, return ZERO
+	 * @return ZERO or int
+	 */
+	int Soupline::getFewestDrinksServedToACustomer() {
+		if (my_customers.empty()) {
+			return ZERO;
+		}
+		int fewest = my_customers[0].numbDrinks;
+		for (customer c : my_customers) {
+			if (c.numbDrinks < fewest) {
+				fewest = c.numbDrinks;
+			}
+		}
+		return fewest;
+	}
